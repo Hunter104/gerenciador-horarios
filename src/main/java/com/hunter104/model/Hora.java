d6939982941200f7608013ca034aa7a89b74b4b2
Hunter104 <pudimdeleite2004@outlook.com>
2023-07-26 13:01:12 -0300

    fix: consertado formatação de horas

--- src/main/java/com/hunter104/model/Hora.java
+++ src/main/java/com/hunter104/model/Hora.java
@@ -1,14 +1,14 @@
 package com.hunter104.model;
 
 public enum Hora {
-    M12("08:00 - 09:00", "12", Turno.MANHA),
-    M34("10:00 - 11:00", "34", Turno.MANHA),
-    M5("12:00 - 13:00", "5", Turno.MANHA),
-    T1("13:00 - 13:00", "1", Turno.TARDE),
-    T23("14:00 - 15:00", "23", Turno.TARDE),
-    T45("16:00 - 17:00", "45", Turno.TARDE),
-    N12("19:00 - 20:00", "12", Turno.NOITE),
-    N34("20:00 - 22:30", "34", Turno.NOITE);
+    M12("08:00 - 09:50", "12", Turno.MANHA),
+    M34("10:00 - 11:50", "34", Turno.MANHA),
+    M5("12:00 - 12:55", "5", Turno.MANHA),
+    T1("12:55 - 13:50", "1", Turno.TARDE),
+    T23("14:00 - 15:50", "23", Turno.TARDE),
+    T45("16:00 - 17:50", "45", Turno.TARDE),
+    N12("19:00 - 20:40", "12", Turno.NOITE),
+    N34("20:50 - 22:30", "34", Turno.NOITE);
 
     private final String nome;
     private final String codigo;
