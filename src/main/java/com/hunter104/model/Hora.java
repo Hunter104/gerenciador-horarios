a0ccdef0b34fa936486a7779c898f238c89c5c45
Hunter104 <pudimdeleite2004@outlook.com>
2023-07-26 08:01:00 -0300

    style: modificado estilo de formatação de horas

--- src/main/java/com/hunter104/model/Hora.java
+++ src/main/java/com/hunter104/model/Hora.java
@@ -1,18 +1,19 @@
 package com.hunter104.model;
 
 public enum Hora {
-    M12("08h às 09h50", "12", Turno.MANHA),
-    M34("10h às 11h50", "34", Turno.MANHA),
-    M5("12h às 13h00", "5", Turno.MANHA),
-    T1("13h às 13h50", "1", Turno.TARDE),
-    T23("14h às 15h50", "23", Turno.TARDE),
-    T45("16h às 17h50", "45", Turno.TARDE),
-    N12("19h às 20h40", "12", Turno.NOITE),
-    N34("20h50 às 22h30", "34", Turno.NOITE);
+    M12("08:00 - 09:00", "12", Turno.MANHA),
+    M34("10:00 - 11:00", "34", Turno.MANHA),
+    M5("12:00 - 13:00", "5", Turno.MANHA),
+    T1("13:00 - 13:00", "1", Turno.TARDE),
+    T23("14:00 - 15:00", "23", Turno.TARDE),
+    T45("16:00 - 17:00", "45", Turno.TARDE),
+    N12("19:00 - 20:00", "12", Turno.NOITE),
+    N34("20:00 - 22:30", "34", Turno.NOITE);
 
     private final String nome;
     private final String codigo;
     private final Turno turno;
+
     Hora(String nome, String codigo, Turno turno) {
         this.nome = nome;
         this.codigo = codigo;
