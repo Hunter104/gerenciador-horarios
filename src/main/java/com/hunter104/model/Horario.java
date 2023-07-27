721f7af300b8d5f8d3c98e5a1fbf648da31fc438
Hunter104 <pudimdeleite2004@outlook.com>
2023-07-27 16:16:01 -0300

    refactor: movida verificação de criação de horário para construtor da classe Horario, e extraidos métodos de parsing

--- src/main/java/com/hunter104/model/Horario.java
+++ src/main/java/com/hunter104/model/Horario.java
@@ -1,6 +1,7 @@
 package com.hunter104.model;
 
 import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -8,38 +9,49 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public record Horario(Set<DiadaSemana> dias, Set<Hora> horas, String horarioCodificado) {
+    public Horario {
+        if (dias.size() == 0) {
+            throw new IllegalArgumentException("Nenhum dia selecionado para criação de horário");
+        }
+        if (horas.size() == 0) {
+            throw new IllegalArgumentException("Nenhuma hora selecionada para criação de horário");
+        }
+    }
+
     public static Horario criarFromCodigo(String horarioCodificado) {
         Pattern pattern = Pattern.compile("^([2-7]+)([MNT])([1-5]+)$", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(horarioCodificado);
+
         if (matcher.find()) {
 
-            String codigoDias = matcher.group(1);
-            String codigoTurno = matcher.group(2);
-            String codigoHorario = matcher.group(3);
+            Set<DiadaSemana> dias = parseDias(matcher.group(1));
 
-            Set<DiadaSemana> dias = Arrays.stream(DiadaSemana.values())
-                    .filter(turno -> codigoDias.contains(String.valueOf(turno.codigo())))
-                    .collect(Collectors.toSet());
+            Turno turno = parseTurno(matcher.group(2));
+            Set<Hora> horas = parseHoras(matcher.group(3), turno);
 
-            Turno turnoAtual = Arrays.stream(Turno.values())
-                    .filter(turnoPossivel -> turnoPossivel.codigo().equals(codigoTurno))
-                    .findFirst()
-                    .orElseThrow();
-
-            Set<Hora> horas = Arrays.stream(Hora.values())
-                    .filter(horarios -> horarios.turno().equals(turnoAtual) && codigoHorario.contains(horarios.codigo()))
-                    .collect(Collectors.toSet());
-
-            boolean nenhumDia = dias.size() == 0;
-            boolean nenhumaHora = horas.size() == 0;
-
-            if (nenhumDia || nenhumaHora) {
-                throw new IllegalArgumentException("código de horário tem formato inválido");
-            }
             return new Horario(dias, horas, horarioCodificado);
         } else {
             throw new IllegalArgumentException("código de horário tem formato inválido");
         }
+    }
+
+    private static Set<Hora> parseHoras(String codigoHoras, Turno turno) {
+        return Arrays.stream(Hora.values())
+                .filter(horarios -> horarios.turno().equals(turno) && codigoHoras.contains(horarios.codigo()))
+                .collect(Collectors.toCollection(() -> EnumSet.noneOf(Hora.class)));
+    }
+
+    private static Set<DiadaSemana> parseDias(String codigoDias) {
+        return Arrays.stream(DiadaSemana.values())
+                .filter(turno -> codigoDias.contains(String.valueOf(turno.codigo())))
+                .collect(Collectors.toCollection(() -> EnumSet.noneOf(DiadaSemana.class)));
+    }
+
+    private static Turno parseTurno(String codigoTurno) {
+        return Arrays.stream(Turno.values())
+                .filter(turnoPossivel -> turnoPossivel.codigo().equals(codigoTurno))
+                .findFirst()
+                .orElseThrow();
     }
 
     public boolean temInterseccao(DiadaSemana dia, Hora hora) {
