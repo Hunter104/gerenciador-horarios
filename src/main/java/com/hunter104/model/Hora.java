47b554bb9ac639c6e250856b2b131e607f63bb76
Hunter104 <pudimdeleite2004@outlook.com>
2023-08-02 08:56:40 -0300

    feature: trocados horários para blocos de uma hora

--- src/main/java/com/hunter104/model/Hora.java
+++ src/main/java/com/hunter104/model/Hora.java
@@ -1,14 +1,21 @@
 package com.hunter104.model;
 
 public enum Hora {
-    M12("08:00 - 09:50", "12", Turno.MANHA),
-    M34("10:00 - 11:50", "34", Turno.MANHA),
+    M1("08:00 - 08:55", "1", Turno.MANHA),
+    M2("08:55 - 09:50", "2", Turno.MANHA),
+    M3("10:00 - 10:55", "3", Turno.MANHA),
+    M4("10:55 - 11:50", "4", Turno.MANHA),
     M5("12:00 - 12:55", "5", Turno.MANHA),
     T1("12:55 - 13:50", "1", Turno.TARDE),
-    T23("14:00 - 15:50", "23", Turno.TARDE),
-    T45("16:00 - 17:50", "45", Turno.TARDE),
-    N12("19:00 - 20:40", "12", Turno.NOITE),
-    N34("20:50 - 22:30", "34", Turno.NOITE);
+    T2("14:00 - 14:55", "2", Turno.TARDE),
+    T3("14:55 - 15:50", "3", Turno.TARDE),
+    T4("16:00 - 16:55", "4", Turno.TARDE),
+    T5("16:55 - 17:50", "5", Turno.TARDE),
+    T6("17:50 - 18:55", "6", Turno.TARDE),
+    N1("19:00 - 19:50", "1", Turno.NOITE),
+    N2("19:50 - 20:40", "2", Turno.NOITE),
+    N3("20:50 - 21:40", "3", Turno.NOITE),
+    N4("21:40 - 22:30", "4", Turno.NOITE);
 
     private final String nome;
     private final String codigo;
